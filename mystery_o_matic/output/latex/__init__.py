72ffcc048b60d2e6716ae122381b905a72e334da
neuromancer <gustavo.grieco@gmail.com>
2024-11-09 18:41:24 +0100

    improve image location support for LaTeX

--- mystery_o_matic/output/latex/__init__.py
+++ mystery_o_matic/output/latex/__init__.py
@@ -121,6 +121,7 @@ def produce_tex_output(static_dir, out_dir, languages, mystery, weapons, weapon_
         args["initialClues"] = initial_clues
         args["additionalClues"] = additional_clues_enumeration
         args["finalLocations"] = get_bullet_list([final_locations_bullets])
+        args["locationImages"] = out_dir + f"/{language}"
         tables = ""
 
         for i in range(len(locations.weapon_locations.items())):
